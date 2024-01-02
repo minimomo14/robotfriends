db84bc903568241219bdb8a5a32368c089c0cc7b
minimomo14 <93943633+minimomo14@users.noreply.github.com>
2024-01-02 14:31:28 -0500

    added ErrorBoundry.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
 import "./App.css";
+import ErrorBoundry from "../components/ErrorBoundry";
 
 class App extends Component {
   constructor() {
@@ -38,7 +39,9 @@ class App extends Component {
           <h1 className="f1">RobotFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-            <CardList robots={filterRobots} />
+            <ErrorBoundry>
+              <CardList robots={filterRobots} />
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
